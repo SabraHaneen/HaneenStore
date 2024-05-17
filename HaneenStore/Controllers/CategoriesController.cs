cb682c3797891ae8dcb4d7ad9f76cd51df17843a
SabraHaneen <sabrahanien@gmail.com>
2024-05-17 20:16:39 +0300

    createCategory

---
+++ HaneenStore/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using HaneenStore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HaneenStore.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        ApplicationDbContext context;
+        public CategoriesController(ApplicationDbContext context
+            )
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ HaneenStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HaneenStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HaneenStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
